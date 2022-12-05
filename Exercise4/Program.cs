f50d60a6c0ec8a7aa95f6635d4a518803ddb59f5
Brilly <ASUS@LAPTOP-BEUE1DIV>
2022-12-05 16:05:21 +0700

    add case 2

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -77,6 +77,14 @@ namespace Exercise4
                         int num = Convert.ToInt32(Console.ReadLine());
                         s.push(num);
                         break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nSTack empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
                 }
             }
         }
